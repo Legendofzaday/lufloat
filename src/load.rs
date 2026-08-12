2ec0af0438f5073a987fea2d8193338f5633725d
Eeswar <eeswarthopuri@gmail.com>
2026-08-12 08:31:25 +0530

    Create load.rs

---
+++ src/load.rs
@@ -0,0 +1,43 @@
+use crate::memory::{Arena, GpuBuffer};
+use std::marker::PhantomData;
+use std::ptr::{NonNull, copy_nonoverlapping, null_mut};
+
+pub fn load_slice<'a>(arena: &'a mut Arena, data: &[u16]) -> Option<GpuBuffer<'a>> {
+    let len: usize = data.len();
+    if len == 0usize {
+        return Some(GpuBuffer {
+            ptr: null_mut::<u16>(),
+            len: 0usize,
+            _marker: PhantomData,
+        });
+    }
+    let byte_size: usize = len.checked_mul(2)?;
+    let raw_ptr: NonNull<u8> = arena.alloc(byte_size)?;
+    let gpu_ptr: *mut u16 = raw_ptr.as_ptr() as *mut u16;
+    unsafe {
+        copy_nonoverlapping(data.as_ptr(), gpu_ptr, len);
+    }
+    Some(GpuBuffer {
+        ptr: gpu_ptr,
+        len,
+        _marker: PhantomData,
+    })
+}
+
+pub fn alloc_uninit<'a>(arena: &'a mut Arena, len: usize) -> Option<GpuBuffer<'a>> {
+    if len == 0usize {
+        return Some(GpuBuffer {
+            ptr: null_mut::<u16>(),
+            len: 0usize,
+            _marker: PhantomData,
+        });
+    }
+    let byte_size: usize = len.checked_mul(2)?;
+    let raw_ptr: NonNull<u8> = arena.alloc(byte_size)?;
+    let gpu_ptr: *mut u16 = raw_ptr.as_ptr() as *mut u16;
+    Some(GpuBuffer {
+        ptr: gpu_ptr,
+        len,
+        _marker: PhantomData,
+    })
+}
