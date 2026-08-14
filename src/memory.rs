15110e48202f5858d1347e112a7733bb19289ace
Eeswar <eeswarthopuri@gmail.com>
2026-08-14 16:12:43 +0530

    Update memory.rs
    improved comments.

--- src/memory.rs
+++ src/memory.rs
@@ -54,7 +54,7 @@ pub struct Arena {
 }
 
 impl Arena {
-    /// Reserve contiguous memory of `capacity` bytes.
+    /// Reserves contiguous memory of `capacity` bytes.
     pub fn new(capacity: usize) -> Self {
         assert!(capacity > 0, "Arena capacity must be greater than 0.");
         let aligned_capacity = capacity
