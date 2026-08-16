34f6a7eaa0acba51d70da56f8a2a69a67c8581d6
Eeswar <eeswarthopuri@gmail.com>
2026-08-16 18:14:13 +0530

    Update negative_mask.rs
    changed abi signature.

--- src/negative_mask.rs
+++ src/negative_mask.rs
@@ -2,7 +2,7 @@ use crate::memory::{UnifiedBuffer, hip_check};
 use std::ffi::c_int;
 
 unsafe extern "C" {
-    fn negative_mask(data: *const u16, size: usize, mask: *mut u16);
+    fn negative_mask(data: *const u16, size: usize, mask: *mut u16) -> c_int;
 }
 
 pub(crate) fn apply<'a>(data: &UnifiedBuffer<'a>, mask: &mut UnifiedBuffer<'a>) {
