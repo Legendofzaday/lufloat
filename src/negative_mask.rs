78cae1d7ff2722481f8e2ac2f0136e7a3d55bb43
Eeswar <eeswarthopuri@gmail.com>
2026-08-16 10:13:34 +0530

    Update negative_mask.rs
    removed unnecessary type annotations.

--- src/negative_mask.rs
+++ src/negative_mask.rs
@@ -9,10 +9,10 @@ pub(crate) fn apply<'a>(data: &UnifiedBuffer<'a>, mask: &mut UnifiedBuffer<'a>) 
         data.len, mask.len,
         "[lufloat error] input and output buffers must have the same length."
     );
-    if data.len == 0usize {
+    if data.len == 0 {
         return;
     }
-    let padded_size: usize = (data.len + 2047usize) & !2047usize;
+    let padded_size = (data.len + 2047) & !2047;
     unsafe {
         negative_mask(data.ptr as *const u16, padded_size, mask.ptr);
     }
