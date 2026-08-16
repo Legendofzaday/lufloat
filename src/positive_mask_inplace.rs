527fc2a15ed1cc989fbd1514b17eda5d736147c3
Eeswar <eeswarthopuri@gmail.com>
2026-08-16 10:14:13 +0530

    Update positive_mask_inplace.rs
    removed unnecessary type annotations.

--- src/positive_mask_inplace.rs
+++ src/positive_mask_inplace.rs
@@ -5,10 +5,10 @@ unsafe extern "C" {
 }
 
 pub(crate) fn apply<'a>(buffer: UnifiedBuffer<'a>) -> UnifiedBuffer<'a> {
-    if buffer.len == 0usize {
+    if buffer.len == 0 {
         return buffer;
     }
-    let padded_size: usize = (buffer.len + 2047usize) & !2047usize;
+    let padded_size = (buffer.len + 2047) & !2047;
     unsafe {
         positive_mask_inplace(buffer.ptr, padded_size);
     }
