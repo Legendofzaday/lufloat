03d7c5239c8ae5205039be0c1fec482e67ea3f5f
Eeswar <eeswarthopuri@gmail.com>
2026-08-17 18:52:59 +0530

    Update positive_mask.rs
    removed automatic alignment.

--- src/positive_mask.rs
+++ src/positive_mask.rs
@@ -6,14 +6,6 @@ unsafe extern "C" {
 }
 
 pub(crate) fn apply<'a>(data: &UnifiedBuffer<'a>, mask: &mut UnifiedBuffer<'a>) {
-    assert_eq!(
-        data.len, mask.len,
-        "[lufloat error] input and output buffers must have the same length."
-    );
-    if data.len == 0 {
-        return;
-    }
-    let padded_size = (data.len + 2047) & !2047;
     let err = unsafe { positive_mask(data.ptr as *const u16, padded_size, mask.ptr) };
     hip_check(err, file!(), line!());
 }
