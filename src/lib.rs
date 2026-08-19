f3e078aaac932e9eaa5971de7e8190836e788817
Eeswar <eeswarthopuri@gmail.com>
2026-08-19 21:01:55 +0530

    Update lib.rs
    improved documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -21,9 +21,9 @@ impl<'a> UnifiedBuffer<'a> {
     /// let mut buffer_a = UnifiedBuffer::new(&arena, 2048);
     /// let mut buffer_b = UnifiedBuffer::new(&arena, 2048);
     /// let input_data = buffer_a.slice_mut();
-    /// input_data[0] = 0b0_01111_0000000000;
+    /// input_data[0] = 0b1_01111_0000000000;
     /// input_data[1] = 0b0_00000_0000000000;
-    /// input_data[2] = 0b1_01111_0000000000;
+    /// input_data[2] = 0b0_01111_0000000000;
     /// buffer_a.negative_mask(&mut buffer_b);
     /// let output_data = buffer_b.slice();
     /// println!("The first 3 elements are: {:?}", &output_data[..3]);
@@ -33,6 +33,21 @@ impl<'a> UnifiedBuffer<'a> {
     }
 
     /// Replaces negatives with 1, positives with 0.
+    ///
+    /// # Examples
+    ///
+    /// ```rust,ignore
+    /// # use lufloat::{Arena, UnifiedBuffer};
+    /// let arena = Arena::new(2048);
+    /// let mut buffer = UnifiedBuffer::new(&arena, 2048);
+    /// let input_data = buffer.slice_mut();
+    /// input_data[0] = 0b1_01111_0000000000;
+    /// input_data[1] = 0b0_00000_0000000000;
+    /// input_data[2] = 0b0_01111_0000000000;
+    /// buffer.negative_mask_inplace();
+    /// let output_data = buffer.slice();
+    /// println!("The first 3 elements are: {:?}", &output_data[..3]);
+    /// ```
     pub fn negative_mask_inplace(&mut self) {
         negative_mask_inplace::apply(self);
     }
@@ -51,9 +66,9 @@ impl<'a> UnifiedBuffer<'a> {
     /// let mut buffer_a = UnifiedBuffer::new(&arena, 2048);
     /// let mut buffer_b = UnifiedBuffer::new(&arena, 2048);
     /// let input_data = buffer_a.slice_mut();
-    /// input_data[0] = 0b0_01111_0000000000;
+    /// input_data[0] = 0b1_01111_0000000000;
     /// input_data[1] = 0b0_00000_0000000000;
-    /// input_data[2] = 0b1_01111_0000000000;
+    /// input_data[2] = 0b0_01111_0000000000;
     /// buffer_a.positive_mask(&mut buffer_b);
     /// let output_data = buffer_b.slice();
     /// println!("The first 3 elements are: {:?}", &output_data[..3]);
@@ -63,6 +78,21 @@ impl<'a> UnifiedBuffer<'a> {
     }
 
     /// Replaces negatives with 0, positives with 1.
+    ///
+    /// # Examples
+    ///
+    /// ```rust,ignore
+    /// # use lufloat::{Arena, UnifiedBuffer};
+    /// let arena = Arena::new(2048);
+    /// let mut buffer = UnifiedBuffer::new(&arena, 2048);
+    /// let input_data = buffer.slice_mut();
+    /// input_data[0] = 0b1_01111_0000000000;
+    /// input_data[1] = 0b0_00000_0000000000;
+    /// input_data[2] = 0b0_01111_0000000000;
+    /// buffer.positive_mask_inplace();
+    /// let output_data = buffer.slice();
+    /// println!("The first 3 elements are: {:?}", &output_data[..3]);
+    /// ```
     pub fn positive_mask_inplace(&mut self) {
         positive_mask_inplace::apply(self);
     }
