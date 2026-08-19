d6e09b4bad72b904cf2eb4f1a7f74b012bfd086d
Eeswar <eeswarthopuri@gmail.com>
2026-08-19 19:53:53 +0530

    Update lib.rs
    sorted functions.

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,10 @@ mod positive_mask_inplace;
 pub use memory::{Arena, UnifiedBuffer};
 
 impl<'a> UnifiedBuffer<'a> {
-    pub fn positive_mask_inplace(self) -> Self {
-        positive_mask_inplace::apply(self)
+    pub fn negative_mask(&self, out_buf: &mut Self) {
+        negative_mask::apply(self, out_buf);
     }
-
+    
     pub fn negative_mask_inplace(self) -> Self {
         negative_mask_inplace::apply(self)
     }
@@ -18,8 +18,8 @@ impl<'a> UnifiedBuffer<'a> {
     pub fn positive_mask(&self, out_buf: &mut Self) {
         positive_mask::apply(self, out_buf);
     }
-
-    pub fn negative_mask(&self, out_buf: &mut Self) {
-        negative_mask::apply(self, out_buf);
+    
+    pub fn positive_mask_inplace(self) -> Self {
+        positive_mask_inplace::apply(self)
     }
 }
