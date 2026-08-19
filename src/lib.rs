658a6a2c5b0cf405faf7a90a5496eb7446fd09c5
Eeswar <eeswarthopuri@gmail.com>
2026-08-19 20:28:22 +0530

    Update lib.rs
    improved documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -38,6 +38,26 @@ impl<'a> UnifiedBuffer<'a> {
     }
 
     /// Converts negatives to 0, positives to 1.
+    ///
+    /// # Panics
+    ///
+    /// * `self.len` is not equal to `out_buf.len`.
+    ///
+    /// # Examples
+    ///
+    /// ```rust,ignore
+    /// # use lufloat::{Arena, UnifiedBuffer};
+    /// let arena = Arena::new(4096);
+    /// let mut buffer_a = UnifiedBuffer::new(&arena, 2048);
+    /// let mut buffer_b = UnifiedBuffer::new(&arena, 2048);
+    /// let input_data = buffer_a.slice_mut();
+    /// input_data[0] = 0b0_01111_0000000000;
+    /// input_data[1] = 0b0_00000_0000000000;
+    /// input_data[2] = 0b1_01111_0000000000;
+    /// buffer_a.positive_mask(&mut buffer_b);
+    /// let output_data = buffer_b.slice();
+    /// println!("The first 3 elements are: {:?}", &output_data[..3]);
+    /// ```
     pub fn positive_mask(&self, out_buf: &mut Self) {
         positive_mask::apply(self, out_buf);
     }
