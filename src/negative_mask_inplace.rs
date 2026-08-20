257d550c0d46cb8bde29f2cc1303dbb3056d4617
Eeswar <eeswarthopuri@gmail.com>
2026-08-20 22:44:48 +0530

    Update negative_mask_inplace.rs
    added safety check.

--- src/negative_mask_inplace.rs
+++ src/negative_mask_inplace.rs
@@ -9,3 +9,29 @@ pub(crate) fn apply(buffer: &mut UnifiedBuffer<'_>) {
     let err = unsafe { negative_mask_inplace(buffer.ptr, buffer.len) };
     hip_check(err, file!(), line!());
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::memory::{Arena, UnifiedBuffer};
+
+    #[test]
+    fn exhaustive_negative_mask_inplace() {
+        let arena = Arena::new(1 << 16);
+        let mut buffer = UnifiedBuffer::new(&arena, 1 << 16);
+        let input_data = buffer.slice_mut();
+        for i in 0..(1 << 16) {
+            input_data[i] = i as u16;
+        }
+        apply(&mut buffer);
+        let output_data = buffer.slice();
+        for i in 0..(1 << 16) {
+            let expected = if (i as u16 & 0x8000) == 0 {
+                0x0000
+            } else {
+                0x3C00
+            };
+            assert_eq!(output_data[i], expected, "Failed at {:016b}", i as u16);
+        }
+    }
+}
