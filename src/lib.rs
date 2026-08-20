f3727a11185a04acf86997fc79dcff732e217e52
Eeswar <eeswarthopuri@gmail.com>
2026-08-20 11:17:35 +0530

    Update lib.rs
    improved documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,21 @@
 //! lufloat = "0.1.0"
 //! ```
 //!
+//! ## Then
+//!
+//! ```rust,ignore
+//! use lufloat::{Arena, UnifiedBuffer};
+//! let arena = Arena::new(2048);
+//! let mut buffer = UnifiedBuffer::new(&arena, 2048);
+//! let input_data = buffer.slice_mut();
+//! input_data[0] = 0b1_01111_0000000000;
+//! input_data[1] = 0b0_00000_0000000000;
+//! input_data[2] = 0b0_01111_0000000000;
+//! buffer.positive_mask_inplace();
+//! let output_data = buffer.slice();
+//! println!("The first 3 elements are: {:?}", &output_data[..3]);
+//! ```
+//!
 //! # License
 //!
 //! lufloat
