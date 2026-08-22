f12c485c4ad7106b37f53a39b18bbffc9088de68
Eeswar <eeswarthopuri@gmail.com>
2026-08-22 19:59:13 +0530

    Update lib.rs
    added relu functions.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ mod negative_mask;
 mod negative_mask_inplace;
 mod positive_mask;
 mod positive_mask_inplace;
+mod relu;
+mod relu_inplace;
 
 pub use memory::{Arena, UnifiedBuffer};
 
@@ -97,5 +99,50 @@ impl<'a> UnifiedBuffer<'a> {
     /// ```
     pub fn positive_mask_inplace(&mut self) {
         positive_mask_inplace::apply(self);
+    }
+
+    /// Converts elements into max(0, element).
+    ///
+    /// # Panics
+    ///
+    /// * `self.len` is not equal to `out_buf.len`.
+    ///
+    /// # Examples
+    ///
+    /// ```rust,ignore
+    /// # use lufloat::{Arena, UnifiedBuffer};
+    /// let arena = Arena::new(4096);
+    /// let mut buffer_a = UnifiedBuffer::new(&arena, 2048);
+    /// let mut buffer_b = UnifiedBuffer::new(&arena, 2048);
+    /// let input_data = buffer_a.slice_mut();
+    /// input_data[0] = 0b1_01111_0000000000;
+    /// input_data[1] = 0b0_00000_0000000000;
+    /// input_data[2] = 0b0_01111_0000000000;
+    /// buffer_a.relu(&mut buffer_b);
+    /// let output_data = buffer_b.slice();
+    /// println!("The first 3 elements are: {:?}", &output_data[..3]);
+    /// ```
+    pub fn relu(&self, out_buf: &mut Self) {
+        relu::apply(self, out_buf);
+    }
+
+    /// Replaces elements with max(0, element).
+    ///
+    /// # Examples
+    ///
+    /// ```rust,ignore
+    /// # use lufloat::{Arena, UnifiedBuffer};
+    /// let arena = Arena::new(2048);
+    /// let mut buffer = UnifiedBuffer::new(&arena, 2048);
+    /// let input_data = buffer.slice_mut();
+    /// input_data[0] = 0b1_01111_0000000000;
+    /// input_data[1] = 0b0_00000_0000000000;
+    /// input_data[2] = 0b0_01111_0000000000;
+    /// buffer.relu_inplace();
+    /// let output_data = buffer.slice();
+    /// println!("The first 3 elements are: {:?}", &output_data[..3]);
+    /// ```
+    pub fn relu_inplace(&mut self) {
+        relu_inplace::apply(self);
     }
 }
