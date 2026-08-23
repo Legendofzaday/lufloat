3dce3a0d40579eb4389441cad97341b525996399
Eeswar <eeswarthopuri@gmail.com>
2026-08-23 11:58:52 +0530

    Update lib.rs
    added abs functions to public api.

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,51 @@ mod relu_inplace;
 pub use memory::{Arena, UnifiedBuffer};
 
 impl<'a> UnifiedBuffer<'a> {
+    /// Converts elements into abs(element).
+    ///
+    /// # Panics
+    ///
+    /// * `self.len` is not equal to `out_buf.len`.
+    ///
+    /// # Examples
+    ///
+    /// ```rust,ignore
+    /// # use lufloat::{Arena, UnifiedBuffer};
+    /// let arena = Arena::new(4096);
+    /// let mut buffer_a = UnifiedBuffer::new(&arena, 2048);
+    /// let mut buffer_b = UnifiedBuffer::new(&arena, 2048);
+    /// let input_data = buffer_a.slice_mut();
+    /// input_data[0] = 0b1_01111_0000000000;
+    /// input_data[1] = 0b0_00000_0000000000;
+    /// input_data[2] = 0b0_01111_0000000000;
+    /// buffer_a.abs(&mut buffer_b);
+    /// let output_data = buffer_b.slice();
+    /// println!("The first 3 elements are: {:?}", &output_data[..3]);
+    /// ```
+    pub fn abs(&self, out_buf: &mut Self) {
+        abs::apply(self, out_buf);
+    }
+
+    /// Replaces elements with abs(element).
+    ///
+    /// # Examples
+    ///
+    /// ```rust,ignore
+    /// # use lufloat::{Arena, UnifiedBuffer};
+    /// let arena = Arena::new(2048);
+    /// let mut buffer = UnifiedBuffer::new(&arena, 2048);
+    /// let input_data = buffer.slice_mut();
+    /// input_data[0] = 0b1_01111_0000000000;
+    /// input_data[1] = 0b0_00000_0000000000;
+    /// input_data[2] = 0b0_01111_0000000000;
+    /// buffer.abs_inplace();
+    /// let output_data = buffer.slice();
+    /// println!("The first 3 elements are: {:?}", &output_data[..3]);
+    /// ```
+    pub fn abs_inplace(&mut self) {
+        abs_inplace::apply(self);
+    }
+    
     /// Converts negatives to 1, positives to 0.
     ///
     /// # Panics
